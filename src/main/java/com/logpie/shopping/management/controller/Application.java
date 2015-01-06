386a5ad3bed989e2c404c91f0ddaacae89dcd03d
Yilei Zhong <zhoyilei@amazon.com>
2015-01-05 19:52:43 -0800

    fix gradle build

---
+++ src/main/java/com/logpie/shopping/management/controller/Application.java
@@ -0,0 +1,23 @@
+// Copyright 2015 logpie.com. All rights reserved.
+package com.logpie.shopping.management.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * This is just for temporary usage. 
+ * TODO: we need remove the application
+ * 
+ * @author zhoyilei
+ *
+ */
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
