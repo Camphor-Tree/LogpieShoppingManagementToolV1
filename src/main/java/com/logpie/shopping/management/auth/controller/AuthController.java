e959471fea1dccd2aff4ab05b2eef4f1ed86fb32
Jiahang Xu <xujiahang@Jiahangs-MacBook-Pro.local>
2015-01-09 14:42:45 -0800

    updated jars for mysql database; created init method for database

--- src/main/java/com/logpie/shopping/management/auth/controller/AuthController.java
+++ src/main/java/com/logpie/shopping/management/auth/controller/AuthController.java
@@ -14,36 +14,33 @@ import com.logpie.shopping.management.auth.logic.Admin;
 import com.logpie.shopping.management.auth.logic.CookieManager;
 
 @Controller
-public class AuthController
-{
-    private static final Logger LOG = Logger.getLogger(AuthController.class);
+public class AuthController {
+	private static final Logger LOG = Logger.getLogger(AuthController.class);
 
-    @RequestMapping(value = "/signin", method = RequestMethod.GET)
-    public ModelAndView showSignInPage(HttpServletRequest request, HttpServletResponse httpResponse)
-    {
-        final ModelAndView signinPage = new ModelAndView("signin");
-        return signinPage;
-    }
+	@RequestMapping(value = "/signin", method = RequestMethod.GET)
+	public ModelAndView showSignInPage(HttpServletRequest request,
+			HttpServletResponse httpResponse) {
+		final ModelAndView signinPage = new ModelAndView("signin");
+		return signinPage;
+	}
 
-    @RequestMapping(value = "/signin", method = RequestMethod.POST)
-    public Object verifyPassword(HttpServletRequest request, HttpServletResponse httpResponse)
-    {
-        LOG.debug("receiving post signin request");
-        final String email = request.getParameter("email");
-        final String password = request.getParameter("password");
-        if (email.equals("1@amazon.com") && password.equals("1"))
-        {
-            final ModelAndView homePage = new ModelAndView("home");
-            final CookieManager cookieManager = new CookieManager();
-            Cookie cookie = cookieManager.setupAuthCookie(new Admin("id", "name", "email", "qq",
-                    "wechat", "phone", "passversion", "idnumber"));
-            httpResponse.addCookie(cookie);
-            return "redirect:/home";
-        }
-        else
-        {
-            return "redirect:/signin";
-        }
+	@RequestMapping(value = "/signin", method = RequestMethod.POST)
+	public Object verifyPassword(HttpServletRequest request,
+			HttpServletResponse httpResponse) {
+		LOG.debug("receiving post signin request");
+		final String email = request.getParameter("email");
+		final String password = request.getParameter("password");
+		if (email.equals("1@amazon.com") && password.equals("1")) {
+			final ModelAndView homePage = new ModelAndView("home");
+			final CookieManager cookieManager = new CookieManager();
+			Cookie cookie = cookieManager.setupAuthCookie(new Admin("id",
+					"name", "email", "qq", "wechat", "phone", "passversion",
+					"idnumber"));
+			httpResponse.addCookie(cookie);
+			return "redirect:/home";
+		} else {
+			return "redirect:/signin";
+		}
 
-    }
+	}
 }
