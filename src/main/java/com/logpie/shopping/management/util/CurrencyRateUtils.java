6c148bf0d374f95728a1bcd23c61734517dc6342
Yilei <zyl.echo@gmail.com>
2015-07-17 00:00:30 -0700

    Fix the currency rate API. Start to use the currencylayer.com

--- src/main/java/com/logpie/shopping/management/util/CurrencyRateUtils.java
+++ src/main/java/com/logpie/shopping/management/util/CurrencyRateUtils.java
@@ -12,27 +12,33 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class CurrencyRateUtils
 {
     private static final Logger LOG = Logger.getLogger(CurrencyRateUtils.class);
-    private static final String sFreeCurrentConverterApiUrl = "http://www.freecurrencyconverterapi.com/api/v3/convert?q=%s_%s&compact=y";
+    // We currently use currencylayer.com
+    private static final String sFreeCurrentConverterApiUrl = "http://apilayer.net/api/live?access_key=46645c90759497801e3c821b14859cc1&source=%s&currencies=%s&format=1";
     private static final String sChineseYuan = "CNY";
     private static final String sUSDollar = "USD";
-    private static final String sValue = "val";
 
     private static Float sCurrencyRate = 6.2f;
 
     public synchronized void refreshCurrencyRate(final String fromCurrency, final String toCurrency)
     {
         LOG.debug("Refreshing currency Rate");
-        final String queryUrl = String
-                .format(sFreeCurrentConverterApiUrl, fromCurrency, toCurrency);
+        final String queryUrl = String.format(sFreeCurrentConverterApiUrl, fromCurrency,
+                toCurrency);
         final String response = SimpleAPIConnection.doGetQuery(queryUrl);
         if (response != null)
         {
             try
             {
+                /**
+                 * { "success":true,
+                 * "terms":"https:\/\/currencylayer.com\/terms",
+                 * "privacy":"https:\/\/currencylayer.com\/privacy",
+                 * "timestamp":1437113654, "source":"USD", "quotes":{
+                 * "USDCNY":6.20785 } }
+                 */
                 final JSONObject responseJSON = new JSONObject(response);
-                final JSONObject rateJSON = responseJSON.getJSONObject(fromCurrency + "_"
-                        + toCurrency);
-                final Double rate = rateJSON.getDouble(sValue);
+                final JSONObject rateJSON = responseJSON.getJSONObject("quotes");
+                final Double rate = rateJSON.getDouble(fromCurrency + toCurrency);
                 sCurrencyRate = NumberUtils.keepTwoDigitsDecimalForFloat(rate.floatValue());
             } catch (Exception e)
             {
@@ -46,8 +52,8 @@ public class CurrencyRateUtils
         return sCurrencyRate;
     }
 
-    // Execute every 1 hour
-    @Scheduled(fixedRate = 1000 * 3600)
+    // Execute every 2 hour
+    @Scheduled(fixedRate = 1000 * 3600 * 2)
     public synchronized void refreshUScurrencyRate()
     {
         refreshCurrencyRate(sUSDollar, sChineseYuan);
